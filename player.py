f851c10a30a0f26f9c522526c3fcd75bf38bfb2e
Dinis-Lei <dinislei@ua.pt>
2023-11-20 14:29:23 +0000

    sprite rotating around its center

--- player.py
+++ player.py
@@ -10,6 +10,8 @@ from bullet import Bullet
 AccelerateMAX = 20
 BrakeMAX = -10
 
+display = pygame.display.set_mode((SCALE * WIDTH, SCALE * HEIGHT))
+
 class Player(Actor):
     def __init__(self, observer: Observer) -> None:
         super().__init__([400,200], PlayerSprite("player3(1).png"))
@@ -31,6 +33,13 @@ class Player(Actor):
         self.collision_box = CollisionCircle(self, self.observer, center=self.rect.center, radius=50)
         self.collision_box.set_enter_func(self.damage_taken)
 
+        # draw circle
+        self.circle = pygame.sprite.Sprite()
+        self.circle.image  = pygame.Surface((99, 99), pygame.SRCALPHA)
+        pygame.draw.circle(self.circle.image, (255, 255, 0, 128), self.rect.center, 50)
+        
+        self.test_angle = 0
+
     def accelerate(self):
         self.velocity += 1 if self.velocity < AccelerateMAX else 0
 
@@ -45,7 +54,7 @@ class Player(Actor):
         self.position[1] += math.sin(self.direction) * self.velocity
         self.pivot[0] += math.cos(self.direction) * self.velocity
         self.pivot[1] += math.sin(self.direction) * self.velocity
-        print(self.pivot)
+        #print(self.pivot)
 
         self.collision_box.check_collision()
 
@@ -74,9 +83,48 @@ class Player(Actor):
         bullet = Bullet(self.observer, self.direction, self.position)
         print("shooting")
     
+
+    def blitRotate(self,image, pos, originPos, angle):
+
+        # offset from pivot to center
+        image_rect = image.get_rect(topleft = (pos[0] - originPos[0], pos[1]-originPos[1]))
+        offset_center_to_pivot = pygame.math.Vector2(pos) - image_rect.center
+        
+        # roatated offset from pivot to center
+        rotated_offset = offset_center_to_pivot.rotate(-angle)
+
+        # rotated image center
+        rotated_image_center = (pos[0] - rotated_offset.x, pos[1] - rotated_offset.y)
+
+        # get a rotated image
+        rotated_image = pygame.transform.rotate(image, angle)
+        rotated_image_rect = rotated_image.get_rect(center = rotated_image_center)
+
+        # # rotate and blit the image
+        # surf.blit(rotated_image, rotated_image_rect)
+    
+        # # draw rectangle around the image
+        # pygame.draw.rect(surf, (255, 0, 0), (*rotated_image_rect.topleft, *rotated_image.get_size()),2)
+        return rotated_image, rotated_image_rect
+
     def display(self):
-        self.rotate()
-        self.sprite.display_sprite(self.position[0] + self.rect.center[0], self.position[1] + self.rect.center[1])
+        #self.rotate()
+        self.test_angle += 2
+        self.sprite.update_sprite(img=pygame.transform.rotate(self.sprite.get_sprite(), int(self.test_angle)%360))
+
+        img, rect = self.blitRotate(self.sprite.get_sprite(), self.position, (49,49), self.test_angle)
+        print(rect)
+        self.rect = self.sprite.get_sprite().get_rect()
+        display.blit(img, rect)
+        #self.sprite.display_sprite(self.position[0] + self.rect[0], self.position[1] + self.rect[1])
+        display.blit(self.circle.image, (self.position[0]-49, self.position[1]-49))
+        print(f"Player position: {self.position}")
+        print(f"Rect center position: {self.rect.center}")
+        print(f"Circle center position: {self.circle.image.get_rect().center}")
+        print(f"Rect position: {self.rect}")
+        print(f"Circle position: {self.circle.image.get_rect()}")
+
+        
         
     
     def damage_taken(self):
